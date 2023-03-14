ded3db5d00c5ce5d63c85edb94d9ac22ade983b8
Daniel Thorp <arcanistzed@gmail.com>
2023-03-14 03:14:40 -0400

    fix: use new repo URL
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import Fade from "./animations/Fade";
 const Logo = () => {
 	return (
 		<Fade transition={{ duration: 5 }}>
-			<img src="/vp-external/IEEE.svg" className="absolute top-8 left-8 w-48" />
+			<img src="/vp-external-dnAtZXh0ZXJuYWw=/IEEE.svg" className="absolute top-8 left-8 w-48" />
 		</Fade>
 	);
 };
