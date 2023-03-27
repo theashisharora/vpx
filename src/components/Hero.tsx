4282a6bf0f545674859c545fb035bfad37f9004e
Daniel Thorp <arcanistzed@gmail.com>
2023-03-27 05:04:10 -0400

    refactor: move images out of folder
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,28 +17,28 @@ const Hero = () => {
 			<Fade transition={{ duration: 1 }}>
 				<div className="-z-10 h-1/4 opacity-30">
 					<img
-						src="/vpx/images/hth.webp"
+						src="/vpx/hth.webp"
 						alt="Hack the Hill"
 						className={`mask absolute inset-0 h-3/4 w-full object-cover transition-opacity duration-500 ${
 							index === 0 ? "z-10 opacity-100" : "-z-10 opacity-0"
 						}`}
 					/>
 					<img
-						src="/vpx/images/web-workshop.webp"
+						src="/vpx/web-workshop.webp"
 						alt="Web Workshop"
 						className={`mask absolute inset-0 h-3/4 w-full object-cover transition-opacity duration-500 ${
 							index === 1 ? "z-10 opacity-100" : "-z-10 opacity-0 "
 						}`}
 					/>
 					<img
-						src="/vpx/images/open-house.webp"
+						src="/vpx/open-house.webp"
 						alt="Open House"
 						className={`mask absolute inset-0 h-3/4 w-full object-cover transition-opacity duration-500 ${
 							index === 2 ? "z-10 opacity-100" : "-z-10 opacity-0"
 						}`}
 					/>
 					<img
-						src="/vpx/images/cybersec.webp"
+						src="/vpx/cybersec.webp"
 						alt="uOttawa Cybersecurity Club"
 						className={`mask absolute inset-0 h-3/4 w-full object-cover transition-opacity duration-500 ${
 							index === 3 ? "z-10 opacity-100" : "-z-10 opacity-0 "
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import Fade from "./animations/Fade";
 const Logo = () => {
 	return (
 		<Fade transition={{ duration: 1 }}>
-			<img src="/vpx/images/IEEE.svg" className="absolute top-8 right-8 w-48" />
+			<img src="/vpx/IEEE.svg" className="absolute top-8 right-8 w-48" />
 		</Fade>
 	);
 };
