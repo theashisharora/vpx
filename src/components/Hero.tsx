21182c814e224c1925827335fee63116be946863
Daniel Thorp <arcanistzed@gmail.com>
2023-03-27 04:25:57 -0400

    feat: image slideshow
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,52 @@
+import { useEffect, useState } from "react";
 import { Fade, Reveal, Slide } from "./animations";
 
 const Hero = () => {
+	const [index, setIndex] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setIndex((index) => (index + 1) % 4);
+		}, 10000);
+
+		return () => clearInterval(interval);
+	}, []);
+
 	return (
 		<header className="flex h-screen flex-col items-center justify-center overflow-x-hidden">
+			<Fade transition={{ duration: 1 }}>
+				<div className="-z-10 h-1/4 opacity-30">
+					<img
+						src="/vpx/images/web-workshop.webp"
+						alt="Web Workshop"
+						className={`mask absolute inset-0 h-3/4 w-full object-cover transition-opacity duration-500 ${
+							index === 0 ? "z-10 opacity-100" : "-z-10 opacity-0 "
+						}`}
+					/>
+					<img
+						src="/vpx/images/cybersec.webp"
+						alt="uOttawa Cybersecurity Club"
+						className={`mask absolute inset-0 h-3/4 w-full object-cover transition-opacity duration-500 ${
+							index === 1 ? "z-10 opacity-100" : "-z-10 opacity-0 "
+						}`}
+					/>
+					<img
+						src="/vpx/images/hth.webp"
+						alt="Hack the Hill"
+						className={`mask absolute inset-0 h-3/4 w-full object-cover transition-opacity duration-500 ${
+							index === 2 ? "z-10 opacity-100" : "-z-10 opacity-0"
+						}`}
+					/>
+					<img
+						src="/vpx/images/open-house.webp"
+						alt="Open House"
+						className={`mask absolute inset-0 h-3/4 w-full object-cover transition-opacity duration-500 ${
+							index === 3 ? "z-10 opacity-100" : "-z-10 opacity-0"
+						}`}
+					/>
+				</div>
+			</Fade>
+
 			<Fade transition={{ duration: 5 }}>
 				<Slide transition={{ delay: 1, duration: 3 }} side="left">
 					<h1 className="text-center text-[5vmin] font-bold text-blue-200">VOTE</h1>
